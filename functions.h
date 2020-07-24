42abe8f010dcd0b686a40672b4378fa2d4640995
sequint <65196919+sequint@users.noreply.github.com>
2020-07-24 06:19:10 -0700

    Update functions.h

--- functions.h
+++ functions.h
@@ -8,12 +8,12 @@ using namespace std;
 
 // Global Variables - constant menu choices
 const int APPEND_CAR = 1,
-		  PLACE_ORDER = 2,
-		  COUNT_CARS = 3,
-		  MOVE_KIDS_TO_FRONT = 4,
-		  MOVE_FRIEND_TO_FRONT = 5,
-		  DISPLAY_QUEUE = 6,
-		  EXIT_PROGRAM = 7;
+	  PLACE_ORDER = 2,
+	  COUNT_CARS = 3,
+	  MOVE_KIDS_TO_FRONT = 4,
+	  MOVE_FRIEND_TO_FRONT = 5,
+	  DISPLAY_QUEUE = 6,
+	  EXIT_PROGRAM = 7;
 
 // Input Functions
 void menu(int&);
